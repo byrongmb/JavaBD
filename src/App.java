54d117598f6ec8ab2474a57e433dbce23d08a0e4
byrongmb <byrongamboa@outlook.com>
2020-09-19 19:56:46 -0500

    Project structure

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import SQLConnector.Connector;
+
+public class App {
+    private static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Ingrese las credenciales");
+
+        System.out.print("Username: ");
+        String username = teclado.nextLine();
+
+        System.out.print("Password: ");
+        String password = teclado.nextLine();
+
+        Connector com = new Connector(username, password);
+        System.out.println("Estado de la Conexion: " + com.isConneted());
+    }
+}
---
+++ src/SQLConnector/Connector.java
@@ -0,0 +1,48 @@
+package SQLConnector;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+    private final String URL;
+    private String username;
+    private String password;
+
+    public boolean isConneted() {
+        boolean band = false;
+        Connection con = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = (Connection) DriverManager.getConnection(URL, username, password);
+            band = true;
+        } catch (final ClassNotFoundException e) {
+            System.out.println("Error!! ClassNotFoundException: " + e.getMessage());
+        } catch (final SQLException e) {
+            System.out.println("Error!! SQLException: " + e.getMessage());
+        }
+        return band;
+    }
+
+    /* Getters and Setters */
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public Connector(final String username, final String password) {
+        setUsername(username);
+        setPassword(password);
+        this.URL = "jdbc:sqlserver://DESKTOP-9L6HT12;databaseName=Personas";
+    }
+}
