ca0091c2355dc11c63e7bc29d1a2c6d018672fa4
byrongmb <byrongamboa@outlook.com>
2020-09-24 14:09:00 -0500

    Loggin Version 2

--- src/Windows/Login.java
+++ src/Windows/Login.java
@@ -5,7 +5,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Login extends JFrame {
-    private JPanel panel = new JPanel();
+    private JPanel[] panel = new JPanel[2];
     private JLabel[] label = new JLabel[3];
     private JTextField[] textfield = new JTextField[2];
     private Buttons[] button = new Buttons[2];
@@ -25,43 +25,55 @@ public class Login extends JFrame {
         buttons();
     }
 
-
     /*Components*/
     private void initializePanel(){
-        this.getContentPane().add(panel);
-        this.panel.setLayout(null);
+        
+        for (int i = 0; i < panel.length; i++) {
+            panel[i] = new JPanel();
+            this.panel[i].setLayout(null);
+        }
+
+        /*Agregando al Panel a la ventana*/
+        this.getContentPane().add(panel[0]);
+        this.panel[0].add(panel[1]);
+
+        this.panel[1].setBackground(new java.awt.Color(245,241,241));
+        this.panel[1].setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        this.panel[1].setBounds(30, 55, 320, 130);
     }
 
     private void labels(){
         for (int i = 0; i < label.length; i++) {
             label[i] = new JLabel();
-            panel.add(label[i]);
         }
 
+        panel[0].add(label[0]);
         label[0].setText("Iniciar Sesión");
         label[0].setBounds(150, 10, 200, 50);
 
+        panel[1].add(label[1]);
         label[1].setText("Username:");
-        label[1].setBounds(50, 70, 200, 50);
+        label[1].setBounds(10, 10, 200, 50);
 
+        panel[1].add(label[2]);
         label[2].setText("Password:");
-        label[2].setBounds(50, 100, 200, 50);
+        label[2].setBounds(10, 40, 200, 50);
     }
 
     private void textfields(){
         for (int i = 0; i < textfield.length; i++) {
             textfield[i] = new JTextField();
-            panel.add(textfield[i]);            
+            panel[1].add(textfield[i]);            
         }
 
-        textfield[0].setBounds(120, 85, 200, 20);
-        textfield[1].setBounds(120, 115, 200, 20);
+        textfield[0].setBounds(75, 25, 200, 20);
+        textfield[1].setBounds(75, 55, 200, 20);
     }
 
     private void buttons(){
         for (int i = 0; i < button.length; i++) {
             button[i] = new Buttons();
-            panel.add(button[i]);            
+            panel[0].add(button[i]);            
         }
         
         button[0].setTitle("cancel");
