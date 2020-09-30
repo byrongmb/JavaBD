4d407b781123a91054f4b71f9bef36a02e131032
byrongmb <byrongamboa@outlook.com>
2020-09-30 17:10:40 -0500

    Cleann

--- src/SQLConnector/ConnectDataSource.java
+++ src/SQLConnector/ConnectDataSource.java
@@ -1,8 +1,27 @@
 package SQLConnector;
-
+/*=====================================================================
+File: 	 ConnectDataSource.java
+Summary: This Microsoft JDBC Driver for SQL Server sample application
+         demonstrates how to connect to a SQL Server database by 
+	     using a data source object.
+---------------------------------------------------------------------
+This file is part of the Microsoft JDBC Driver for SQL Server Code Samples.
+Copyright (C) Microsoft Corporation.  All rights reserved.
+ 
+This source code is intended only as a supplement to Microsoft
+Development Tools and/or on-line documentation.  See these other
+materials for detailed information regarding Microsoft code samples.
+ 
+THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+PARTICULAR PURPOSE.
+=====================================================================*/
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 public class ConnectDataSource {
@@ -19,7 +38,7 @@ public class ConnectDataSource {
 
         try (Connection con = ds.getConnection();
                 CallableStatement cstmt = con.prepareCall("{call dbo.uspGetEmployeeManagers(?)}");) {
-            System.out.println("Conectado");
+                    System.out.println("Conectado!!");
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
