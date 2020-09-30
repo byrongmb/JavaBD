13c10b9557f91e6022934b5560403ac9b910a4a5
byrongmb <byrongamboa@outlook.com>
2020-09-29 23:07:26 -0500

    Java Docs Version 1

--- src/SQLConnector/Connector.java
+++ src/SQLConnector/Connector.java
@@ -1,3 +1,14 @@
+/**
+ * <h1>Clase Conector<h1>
+ * Clase de ejemplo para realizar conexiones con SQL Server a traves de JDBC
+ * <p>
+ * La clase cuenta metodos para verificar conexion, guardar datas y obtener, 
+ * datos de una Base de datos en SQL Server
+ * 
+ * @author byrongamboa
+ * @version 3.2
+ * @since 20-09-2020
+ */
 package SQLConnector;
 import java.sql.*;
 
@@ -10,11 +21,27 @@ public class Connector {
 
     private Connection con = null;
 
+    /**
+    * Este metodo, realiza la conexion a la Base de datos
+    * @exception ClassNotFoundException cuando hay problemas en el driver
+    * @exception SQLException cuando hay problemas a iniciar sesion
+    * 
+    * @see ClassNotFoundException
+    * @see SQLException
+    */
     public void isConneted() throws ClassNotFoundException, SQLException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         setCon((Connection) DriverManager.getConnection(URL, username, password));
     }
 
+    /**
+    * Este metodo, guarada los datos a la base de datos
+    * @exception SQLException cuando hay problemas en la sesion
+    * @param data Recibe un arreglo de String con los datos colocados
+    * @return Retorna 0 cuando hubo problemas a guardar y 1 cuando se guardo correctamente
+    * 
+    * @see SQLException
+    */
     public int saveData(String[] data) throws SQLException {
         int band = 0;
         String query = "insert into "+ table +"(Nombre, Apellido, Edad, Telefono) values(?,?,?,?)";
@@ -27,6 +54,14 @@ public class Connector {
         return band;
     }
 
+    /**
+    * Este metodo, devuelve los datos guardados en una base de datos
+    * @exception SQLException cuando hay problemas en la sesion
+    * @return Retorna un tipo de dato {@code ResultSet}
+    * 
+    * @see SQLException
+    * @see ResultSet
+    */
     public ResultSet getData() throws SQLException {
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM datos");
